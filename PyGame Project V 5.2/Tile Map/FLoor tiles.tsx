33e7e51c80b797f72fc46b0685fbbb7f77d0b86f
bruhenri12 <61971775+bruhenri12@users.noreply.github.com>
2021-04-30 02:10:44 -0300

    Add files via upload

---
+++ PyGame Project V 5.2/Tile Map/FLoor tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="FLoor tiles" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="terrain_atlas.png" width="1024" height="1024"/>
+</tileset>
